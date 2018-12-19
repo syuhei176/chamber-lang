9c741e8e8dc14aa804ec5f32afd0aa72cbb358ca
Sg <shogo1104@gmail.com>
2018-12-19 12:45:28 +0900

    test failing but look for some design

--- test/generate_test.js
+++ test/generate_test.js
@@ -9,7 +9,6 @@ const generator = require('../lib/index');
 describe('generate', function() {
 
   describe('generate solidity', function() {
-
     it('should parse the chamber lang and generate solidity source code', function(done) {
       const src = fs.readFileSync(path.join(__dirname, '../examples/SimpleToken.chr'));
       const output = generator(src, 'sol');
@@ -17,7 +16,6 @@ describe('generate', function() {
       fs.writeFileSync(path.join(__dirname, '../examples/SimpleToken.sol'), output);
       done();
     });
-
   })
 
   describe('generate javascript', function() {
@@ -28,6 +26,26 @@ describe('generate', function() {
       fs.writeFileSync(path.join(__dirname, '../examples/SimpleToken.js'), output);
       done();
     });
-
   })
+
+  describe('generate solidity', function() {
+    it('should parse the chamber lang and generate solidity source code', function(done) {
+      const src = fs.readFileSync(path.join(__dirname, '../examples/SellOrder.chr'));
+      const output = generator(src, 'sol');
+      assert.equal(output.toString().length > 0, true);
+      fs.writeFileSync(path.join(__dirname, '../examples/SellOrder.sol'), output);
+      done();
+    });
+  })
+
+  describe('generate javascript', function() {
+    it('should parse the chamber lang and generate JavaScript source code', function(done) {
+      const src = fs.readFileSync(path.join(__dirname, '../examples/SellOrder.chr'));
+      const output = generator(src, 'js');
+      assert.equal(output.toString().length > 0, true);
+      fs.writeFileSync(path.join(__dirname, '../examples/SellOrder.js'), output);
+      done();
+    });
+  })
+
 });

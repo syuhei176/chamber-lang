24de6fffe6742b5db6ba79637352cb682a20a518
Sg <shogo1104@gmail.com>
2018-12-22 13:02:42 +0900

    try embedding annotation info inside JS verifier in order to know how the behavior of the StatefulTx(Contract) must be after Withdrawal

--- examples/LifeCycle.js
+++ examples/LifeCycle.js
@@ -5,14 +5,27 @@ class RPS {
     * @description RPS.commit
     */
     static function commit(tx) {
-  
+      /*
+        [annotations]
+        
+          continuable  (bytes hashedHandB => bytes), (bytes hashedHandA => bytes)
+        
+      */
+      
     }
   
     /**
     * @description RPS.reveal
     */
     static function reveal(tx) {
-  
+      /*
+        [annotations]
+        
+          finish  
+          justsig-required  
+        
+      */
+      
     }
   
   

8b6c275df1877c85f84d7be8486cb7dd9a69133b
Sg <shogo1104@gmail.com>
2018-12-22 12:29:55 +0900

    Refactoring for complex logic

--- examples/LifeCycle.js
+++ examples/LifeCycle.js
@@ -1,6 +1,20 @@
 class RPS {
 
   
+    /**
+    * @description RPS.commit
+    */
+    static function commit(tx) {
+  
+    }
+  
+    /**
+    * @description RPS.reveal
+    */
+    static function reveal(tx) {
+  
+    }
+  
   
   }
   
--- examples/LifeCycle.sol
+++ examples/LifeCycle.sol
@@ -1,5 +1,25 @@
 contract RPS {
 
 
+  /**
+  * @dev RPS.commit
+  */
+  function commit(Tx transaction)
+  internal
+  pure
+  {
+
+  }
+
+  /**
+  * @dev RPS.reveal
+  */
+  function reveal(Tx transaction)
+  internal
+  pure
+  {
+
+  }
+
 
 }
